69d4f351a071d40d3d16ff829911dbb21b1cd3cf
Markus Thiede <mailtothemaru@gmail.com>
2025-11-08 00:33:35 +0100

    added detection of block markdown elements

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,3 +1,16 @@
+from enum import Enum
+import re
+
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
+
 def markdown_to_blocks(markdown: str) -> list[str]:
     blocks = markdown.split("\n\n")
     sanitized_blocks = []
@@ -8,3 +21,36 @@ def markdown_to_blocks(markdown: str) -> list[str]:
             sanitized_blocks.append(sanitized_block)
 
     return sanitized_blocks
+
+
+def block_to_block_type(block: str) -> BlockType:
+    if bool(re.match(r"^#{1,6} .+$", block)):
+        return BlockType.HEADING
+    multiline = block.split("\n")
+    if (
+        len(multiline) > 1
+        and multiline[0].startswith("```")
+        and multiline[-1].startswith("```")
+    ):
+        return BlockType.CODE
+    all_quote = True
+    all_ul = True
+    all_ol = True
+    ol_counter = 1
+    for line in multiline:
+        if not line.startswith(">"):
+            all_quote = False
+        if not line.startswith("-"):
+            all_ul = False
+        if not line.startswith(f"{ol_counter}."):
+            all_ol = False
+        ol_counter += 1
+
+    if all_quote:
+        return BlockType.QUOTE
+    if all_ul:
+        return BlockType.UNORDERED_LIST
+    if all_ol:
+        return BlockType.ORDERED_LIST
+
+    return BlockType.PARAGRAPH
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,6 +1,6 @@
 import unittest
 
-from block_markdown import markdown_to_blocks
+from block_markdown import BlockType, block_to_block_type, markdown_to_blocks
 
 
 class TestMarkdownToBlocks(unittest.TestCase):
@@ -85,3 +85,105 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items",
             ],
         )
+
+
+class TestBlockToBlockType(unittest.TestCase):
+    def test_is_paragraph(self):
+        block = "just a\nparagraph"
+        block_type = block_to_block_type(block)
+        self.assertEqual(BlockType.PARAGRAPH, block_type)
+
+    def test_is_headline(self):
+        h1 = "# headline"
+        h2 = "## headline"
+        h3 = "### headline"
+        h4 = "#### headline"
+        h5 = "##### headline"
+        h6 = "###### headline"
+        h7 = "####### not a headline"
+        no_h = "some text"
+
+        self.assertEqual(BlockType.HEADING, block_to_block_type(h1))
+        self.assertEqual(BlockType.HEADING, block_to_block_type(h2))
+        self.assertEqual(BlockType.HEADING, block_to_block_type(h3))
+        self.assertEqual(BlockType.HEADING, block_to_block_type(h4))
+        self.assertEqual(BlockType.HEADING, block_to_block_type(h5))
+        self.assertEqual(BlockType.HEADING, block_to_block_type(h6))
+        self.assertNotEqual(BlockType.HEADING, block_to_block_type(h7))
+        self.assertNotEqual(BlockType.HEADING, block_to_block_type(no_h))
+
+    def test_is_code_block(self):
+        code = "```\nsome code\n```"
+        block_type = block_to_block_type(code)
+        self.assertEqual(BlockType.CODE, block_type)
+
+        no_code = "``\nsome string\n``"
+        no_code2 = "```\nno code"
+        block_type = block_to_block_type(no_code)
+        block_type2 = block_to_block_type(no_code2)
+        self.assertNotEqual(BlockType.CODE, block_type)
+        self.assertNotEqual(BlockType.CODE, block_type2)
+
+        ml_code = "```\nsome\n more \n code\n```"
+        block_type = block_to_block_type(ml_code)
+        self.assertEqual(BlockType.CODE, block_type)
+
+    def test_is_quote(self):
+        quote = ">this is a quote"
+        quote2 = "> also quote"
+        no_quote = "<no quote"
+        no_quote2 = "< no quote"
+        multiline_quote = "> ml\n> quote"
+        not_ml_quote = "> no\n< quote"
+
+        quote_block = block_to_block_type(quote)
+        quote_block2 = block_to_block_type(quote2)
+        no_quote_block = block_to_block_type(no_quote)
+        no_quote_block2 = block_to_block_type(no_quote2)
+        ml_quote_block = block_to_block_type(multiline_quote)
+        not_ml_quote_block = block_to_block_type(not_ml_quote)
+
+        self.assertEqual(BlockType.QUOTE, quote_block)
+        self.assertEqual(BlockType.QUOTE, quote_block2)
+        self.assertNotEqual(BlockType.QUOTE, no_quote_block)
+        self.assertNotEqual(BlockType.QUOTE, no_quote_block2)
+        self.assertEqual(BlockType.QUOTE, ml_quote_block)
+        self.assertNotEqual(BlockType.QUOTE, not_ml_quote_block)
+
+    def test_is_ul(self):
+        ul = "-this is a ul"
+        ul2 = "- also ul"
+        no_ul = "no ul"
+        multiline_ul = "- ml\n- ul"
+        not_ml_ul = "- no\n ul"
+
+        ul_block = block_to_block_type(ul)
+        ul_block2 = block_to_block_type(ul2)
+        no_ul_block = block_to_block_type(no_ul)
+        ml_ul_block = block_to_block_type(multiline_ul)
+        not_ml_ul_block = block_to_block_type(not_ml_ul)
+
+        self.assertEqual(BlockType.UNORDERED_LIST, ul_block)
+        self.assertEqual(BlockType.UNORDERED_LIST, ul_block2)
+        self.assertNotEqual(BlockType.UNORDERED_LIST, no_ul_block)
+        self.assertEqual(BlockType.UNORDERED_LIST, ml_ul_block)
+        self.assertNotEqual(BlockType.UNORDERED_LIST, not_ml_ul_block)
+
+    def test_is_ol(self):
+        ol = "1.this is a ol"
+        ol2 = "1. also ol"
+        no_ol = "no ol"
+        multiline_ol = "1. ml\n2. ol"
+        not_ml_ol = "1. no\n3. ol"
+
+        ol_block = block_to_block_type(ol)
+        ol_block2 = block_to_block_type(ol2)
+        no_ol_block = block_to_block_type(no_ol)
+        ml_ol_block = block_to_block_type(multiline_ol)
+        not_ml_ol_block = block_to_block_type(not_ml_ol)
+
+        self.assertEqual(BlockType.ORDERED_LIST, ol_block)
+        self.assertEqual(BlockType.ORDERED_LIST, ol_block2)
+        self.assertNotEqual(BlockType.ORDERED_LIST, no_ol_block)
+        self.assertEqual(BlockType.ORDERED_LIST, ml_ol_block)
+        self.assertNotEqual(BlockType.ORDERED_LIST, not_ml_ol_block)
